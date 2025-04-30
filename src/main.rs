ce8af21bf2b0cc6926502b2f6e64b0e1833806f1
Teemu Pätsi <frendsick@gmail.com>
2025-05-01 01:14:12 +0300

    feat: Read test file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::path::Path;
+
+const CODE_FILE: &str = "test.stak";
+
 fn main() {
-    println!("Hello, world!");
+    Path::new(env!("CARGO_MANIFEST_DIR")).join(CODE_FILE);
+    let code = std::fs::read_to_string(CODE_FILE).unwrap();
 }
