cdd3861fa216f2d2b4f47cc4a010d91f5ddf8e71
Teemu Pätsi <frendsick@gmail.com>
2025-05-01 13:16:58 +0300

    feat: Compile and link the generated assembly file

---
+++ src/compile.rs
@@ -0,0 +1,46 @@
+use std::io;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+pub fn compile_assembly_code(asm_path: &Path) -> io::Result<()> {
+    // Derive paths from assembly file
+    let stem = asm_path
+        .file_stem()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid assembly file name"))?;
+    let obj_path = PathBuf::from(format!("{}.o", stem.to_string_lossy()));
+    let exe_path = PathBuf::from(format!("{}", stem.to_string_lossy()));
+
+    // Compile and link
+    compile_with_as(asm_path, &obj_path)?;
+    link_with_ld(&obj_path, &exe_path)?;
+
+    Ok(())
+}
+
+fn compile_with_as(asm_path: &Path, obj_path: &Path) -> io::Result<()> {
+    let status = Command::new("as")
+        .args(["-g", "-o"])
+        .arg(obj_path)
+        .arg(asm_path)
+        .status()?;
+
+    if !status.success() {
+        return Err(io::Error::new(io::ErrorKind::Other, "Compiling failed"));
+    }
+
+    Ok(())
+}
+
+fn link_with_ld(obj_path: &Path, exe_path: &Path) -> io::Result<()> {
+    let status = Command::new("ld")
+        .args(["-melf_x86_64", "-o"])
+        .arg(exe_path)
+        .arg(obj_path)
+        .status()?;
+
+    if !status.success() {
+        return Err(io::Error::new(io::ErrorKind::Other, "Linking failed"));
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![feature(if_let_guard)]
 
 mod asm;
+mod compile;
 mod defs;
 mod ir;
 mod lexer;
 
+use compile::compile_assembly_code;
+use std::io;
 use std::path::Path;
 
 const ASSEMBLY_FILE: &str = "test.asm";
 const CODE_FILE: &str = "test.stak";
 
-fn main() {
+fn main() -> io::Result<()> {
     let crate_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let code = std::fs::read_to_string(crate_dir.join(CODE_FILE)).unwrap();
 
@@ -21,6 +24,11 @@ fn main() {
     dbg!(&ops);
 
     let assembly_code = asm::generate_assembly_code(&ops);
-    std::fs::write(crate_dir.join(ASSEMBLY_FILE), assembly_code);
+    let assembly_file = crate_dir.join(ASSEMBLY_FILE);
+    std::fs::write(&assembly_file, &assembly_code);
     println!("{}", &assembly_code);
+
+    compile_assembly_code(&assembly_file)?;
+
+    Ok(())
 }
